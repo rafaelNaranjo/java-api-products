4dd52ce007731b22ba4c0f2df2ac3a18aa7b38e6
rafaelnaranjo <rrnp.naranjo@gmail.com>
2023-06-21 22:40:33 -0500

    add example to some utilities in junit 5

--- src/test/java/com/rafael/app/product/model/ModelsTest.java
+++ src/test/java/com/rafael/app/product/model/ModelsTest.java
@@ -1,11 +1,33 @@
 package com.rafael.app.product.model;
 
 import com.rafael.app.utils.TestPojos;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * Test instance por clase se realiza para metodos beforeAll y afeter All dejen de ser estaticos
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ModelsTest {
+
+    @BeforeAll
+    void beforeAllTest() throws Exception {
+
+    }
+
+    @AfterAll
+    void afterAllTest() throws Exception {
+
+    }
+
+    @AfterEach
+    void afterEachTest() throws Exception {
+
+    }
+    @BeforeEach
+    void beforeEachTest() throws Exception {
+
+    }
 
     @Test
     void testModelsProduct() throws Exception {
--- src/test/java/com/rafael/app/product/rest/ProductRestControllerTest.java
+++ src/test/java/com/rafael/app/product/rest/ProductRestControllerTest.java
@@ -3,8 +3,14 @@ package com.rafael.app.product.rest;
 import com.rafael.app.product.model.Product;
 import com.rafael.app.product.model.ProductFilters;
 import com.rafael.app.product.service.ProductService;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import lombok.Value;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mybatis.spring.boot.test.autoconfigure.AutoConfigureMybatis;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -12,6 +18,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.time.Month;
 import java.util.ArrayList;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -37,5 +44,40 @@ class ProductRestControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk());
     }
+
+    /*@Test
+    @EnabledOnOs({OS.LINUX, OS.WINDOWS})
+    @DisabledOnOs({OS.MAC})
+    @EnabledOnJre(JRE.JAVA_11)
+    @EnabledIfSystemProperty(named = "java.version", matches = "11")
+    @EnabledIfEnvironmentVariable(named = "", matches = "")
+    void testJunit5SomeUtilsTags() throws Exception {
+
+    }
+
+    @Nested
+    @DisplayName("Product API specifict topics")
+    class ProductControllerSpecifictTest {
+
+        @Test
+        void testSomeScenary() throws Exception {
+
+        }
+        @DisplayName("Product API specifict")
+        @RepeatedTest(value = 5, name = "{displayName} - ProductControllerSpecifict {currentRepetition} de {totalRepetitions}")
+        void testSomeScenary(RepetitionInfo info) throws Exception {
+
+        }
+        @Tag("") indentificacion de test by tag
+        @DisplayName("any")
+        @ParameterizedTest(name = "{index} {0} - {argumentsWithNames}}")
+        @ValueSource
+        @CsvFileSource(resources = "", numLinesToSkip = 1)
+        @EnumSource(value = Month.class, names = {"month", ""})
+        @MethodSource("methodToProduceData")
+        void parameterizedTest() throws Exception {
+
+        }
+    }*/
 
 }
